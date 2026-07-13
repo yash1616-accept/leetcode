aa4ff244b77225f9583ae30f5ec99469e5b86213
yash1616-accept <gaikwadyash964@gmail.com>
2026-07-14 00:20:11 +0530

    Time: 1 ms (98.55%), Space: 44.8 MB (78.66%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,21 +1,45 @@
 class Solution {
+    public int minimum(int a,int b){
+        if(a<b){
+            return a;
+        }else{
+            return b;
+        }
+    }
     public int[] intersect(int[] nums1, int[] nums2) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        ArrayList<Integer> arr = new ArrayList<>();
+        int ans[] = new int[minimum(nums1.length,nums2.length)];
+        int freq[] = new int[1001];
         for(int i=0;i<nums1.length;i++){
-            int a = nums1[i];
-            map.put(a,map.getOrDefault(a,0)+1);
+            freq[nums1[i]]++;
         }
+        int j=0;
         for(int i=0;i<nums2.length;i++){
-            if(map.containsKey(nums2[i])&& map.get(nums2[i])>0){
-                arr.add(nums2[i]);
-                map.put(nums2[i],map.get(nums2[i])-1);
-            }
+         if(freq[nums2[i]]>0){
+            ans[j]=nums2[i];
+         j++;
+            freq[nums2[i]]--;
+         }   
+
         }
-        int ans[]= new int[arr.size()];
-        for(int i=0;i<arr.size();i++){
-            ans[i]=arr.get(i);
-        }
-        return ans;
+        return Arrays.copyOf(ans,j);
+
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // ArrayList<Integer> arr = new ArrayList<>();
+        // for (int i = 0; i < nums1.length; i++) {
+        //     int a = nums1[i];
+        //     map.put(a, map.getOrDefault(a, 0) + 1);
+        // }
+        // for (int i = 0; i < nums2.length; i++) {
+        //     if (map.containsKey(nums2[i]) && map.get(nums2[i]) > 0) {
+        //         arr.add(nums2[i]);
+        //         map.put(nums2[i], map.get(nums2[i]) - 1);
+        //     }
+        // }
+        // int ans[] = new int[arr.size()];
+        // for (int i = 0; i < arr.size(); i++) {
+        //     ans[i] = arr.get(i);
+        // }
+        // return ans;
     }
 }
