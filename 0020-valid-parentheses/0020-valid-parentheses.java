01dd0b57ada59f787de517ca87acbd4c03422608
yash1616-accept <gaikwadyash964@gmail.com>
2026-08-26 12:06:32 +0530

    Time: 3 ms (86.11%), Space: 43.2 MB (73.09%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            } else {
+                if (st.isEmpty())
+                    return false;
+                char top = st.pop();
+                if (ch == ')' && top != '(')
+                    return false;
+                if (ch == ']' && top != '[')
+                    return false;
+                if (ch == '}' && top != '{')
+                    return false;
+            }
+        }
+        return st.isEmpty();
+
+    }
+}
