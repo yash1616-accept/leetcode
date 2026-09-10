4658118ad48de6f1110c42f6fb05a215229b401f
yash1616-accept <gaikwadyash964@gmail.com>
2026-09-10 13:26:30 +0530

    Time: 3 ms (1.69%), Space: 45.1 MB (62.74%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -1,5 +1,27 @@
 class Solution {
     public boolean uniformArray(int[] nums1) {
-     return true;
+     boolean caneven=true;
+     boolean canodd=true;
+     for(int i=0;i<nums1.length;i++ ){
+        boolean evenpossible=nums1[i]%2==0;
+        boolean oddpossible=nums1[i]%2!=0;
+        for(int j=0;j<nums1.length;j++){
+            if(i==j)continue;
+            int diff=nums1[i]-nums1[j];
+            if(diff%2==0){
+                evenpossible=true;
+            }
+            if(diff%2!=0){
+                oddpossible=true;
+            }
+        }
+        if(!evenpossible){
+            caneven=false;
+        }
+        if(!oddpossible){
+        canodd=false;
+        }
+     }
+     return caneven||canodd;
     }
 }
