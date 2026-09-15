c761b1d717537ffeeb5fd6aa33cd033e0e2a3e11
yash1616-accept <gaikwadyash964@gmail.com>
2026-09-15 11:35:06 +0530

    Time: 2317 ms (5.03%), Space: 183.1 MB (9.55%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -9,29 +9,43 @@ class Solution {
         return true;
     }
 
-    int solve(String s,int k ,int i,int j,int dp[][]){
-        int n=s.length();
-     if(i>=n||j>=n)return 0;
-     if(dp[i][j]!=-1)return dp[i][j];
-       if(isPalindrome(s,i,j)){
-      int  take=1+solve(s,k,j+1,j+k,dp);
-      int grow=solve(s,k,i,j+1,dp);
-      int slide= solve(s,k,i+1,j+1,dp);
-       return dp[i][j]=Math.max(take,Math.max(grow,slide));
-       }
-       int grow= solve(s,k,i,j+1,dp);
-       int slide = solve(s,k,i+1,j+1,dp);
-       return dp[i][j]= Math.max(grow,slide);
+    // int solve(String s,int k ,int i,int j,int dp[][]){
+    //     int n=s.length();
+    //  if(i>=n||j>=n)return 0;
+    //  if(dp[i][j]!=-1)return dp[i][j];
+    //    if(isPalindrome(s,i,j)){
+    //   int  take=1+solve(s,k,j+1,j+k,dp);
+    //   int grow=solve(s,k,i,j+1,dp);
+    //   int slide= solve(s,k,i+1,j+1,dp);
+    //    return dp[i][j]=Math.max(take,Math.max(grow,slide));
+    //    }
+    //    int grow= solve(s,k,i,j+1,dp);
+    //    int slide = solve(s,k,i+1,j+1,dp);
+    //    return dp[i][j]= Math.max(grow,slide);
 
-    }
+    // }
 
     public int maxPalindromes(String s, int k) {
-        int n =s.length();
+        int n =s.length();//bottom up approach 
         if(k==1)return n;
-        int [][] dp= new int [n+1][n+1];
-        for(int []row:dp){
-            Arrays.fill(row,-1);
+         int [][] dp= new int [n+1][n+1];
+        // for(int []row:dp){
+        //     Arrays.fill(row,-1);
+        // }
+        for(int i=n-1;i>=0;i--){
+            for(int j =n-1;j>=0;j--){
+                if(isPalindrome(s,i,j)){
+                    int take= 1+(j+k<=n ? dp[j+1][j+k] : 0);//j+k can go out of bound 
+                    int grow= dp[i][j+1];
+                    int slide = dp[i+1][j+1];
+                    dp[i][j]=Math.max(take,Math.max(grow,slide));
+                }else{
+                    int grow =dp[i][j+1];
+                    int slide = dp[i+1][j+1];
+                    dp[i][j]=Math.max(dp[i][j],Math.max(grow,slide));
+                }
+            }
         }
-        return solve(s, k, 0, k - 1,dp);//0=i,k-1=j
+            return dp[0][k-1];
     }
 }
