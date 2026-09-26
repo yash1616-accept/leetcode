91ed38ca5cc63b71b8f39809301939c5a28bdafd
yash1616-accept <gaikwadyash964@gmail.com>
2026-09-26 13:32:00 +0530

    Time: 7 ms (72.27%), Space: 49.7 MB (46.91%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map = new HashMap<>();
+        
+        for(String str :strs){
+            char ch [] = str.toCharArray();
+             Arrays.sort(ch);
+             String key = new String (ch);
+             if(!map.containsKey(key)){
+                map.put(key,new ArrayList<>());
+             }
+             map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
